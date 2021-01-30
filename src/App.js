3e71a6dc1e032e424d3bf90a3f8bc63234dab16a
Aku Paananen <akpaan@utu.fi>
2021-01-30 14:27:50 +0200

    Lisäsin kaikki tiedostot

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ const app = new Clarifai.App({
 class App extends Component {
   //Konstruktori
   constructor() {
-    super();
+    super()
     this.state = {
       input: '',
       imageUrl: '',
-      boxes: []
+      face_boxes: [],
+      message: false
     }
   }
 
@@ -36,13 +37,24 @@ class App extends Component {
   }
 
   //Tulostetaan laatikot
-  displayFaceBox = (boxes) => {
-    console.log(boxes)
-    this.setState({boxes: boxes})
+  displayFaceBox = (face_boxes) => {
+    console.log(face_boxes)
+    this.setState({face_boxes: face_boxes})
   }
 
+  //Hoitaa napin klikkaamisen
   onInputChange = (event) => {
     this.setState({input: event.target.value});
+  }
+
+  //Tulostaa käyttöohjeen
+  onButtonClickShow = () => {
+    this.setState({message: true});
+  }
+
+  //Piilottaa käyttöohjeen
+  onButtonClickHide = () => {
+    this.setState({message: false});
   }
 
   //Kun painetaan lähetä nappia otetaan käyttöön API
@@ -56,19 +68,19 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
-  //Otetaan käyttöön kaikki komponentit
+  //Sisältää sivun rakenteen ja otetaan käyttöön kaikki komponentit
   render() {
     return (
       <div className="App">
         <div>
-          <p className='f3 white'>
-                  {'Tämän sivun tarkoitus on tunnistaa kasvot kuvista.'}
-              </p>
-              <p className='i f4 white'>
-                  {'Liitä kuvan linkki tekstikenttään.'}
-          </p>
-          <input className='f4 pa2 w-30 center' type='text' onChange={this.onInputChange} />
-          <FaceRecognition boxes={this.state.boxes} imageUrl={this.state.imageUrl} />
+          <h1 className='white'>FaceDetector</h1>
+          {this.state.message && <p className='white f5'>Liitä kuvan linkki tekstikenttään. Linkissä täytyy olla .jpg-pääte.</p>}
+          <div className='pv2'>
+            <button className='ph3 white bg-mid-gray' onClick={this.onButtonClickShow}>Käyttöohje</button>
+            <button className='ph3 white bg-mid-gray' onClick={this.onButtonClickHide}>Piilota Käyttöohje</button>
+          </div>
+          <input className='f4 pa2 w-30 center' type='text' placeholder='Linkki tähän' onChange={this.onInputChange} />
+          <FaceRecognition face_boxes={this.state.face_boxes} imageUrl={this.state.imageUrl} />
           <button className='w-30 grow f4 link ph3 pv2 dib white bg-mid-gray' onClick={this.onSubmit}>Tunnista</button>
         </div>
       </div>
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, boxes }) => {
+//Komponentti tulostaa kuvan ja tulostaa kasvojen ympärille tulevat laatikot
+const FaceRecognition = ({ imageUrl, face_boxes }) => {
     return (
         <div className='center'>
             <div className='relative mt2 mb2'>            
                 <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
                 {
-                    boxes.map((box, i) => {
+                    face_boxes.map((box, i) => {
                         const { topRow, rightCol, bottomRow, leftCol } = box;
                         return (<div key={i} id='face' className='bounding-box' style={{top: topRow, right: rightCol, bottom: bottomRow, left: leftCol}}></div>);
                     })
